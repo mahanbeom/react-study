0c64301ed23492a2f06e0595fd4f996dad124628
mahanbeom <akgksqja@gmail.com>
2023-01-02 21:04:04 +0900

    feat: Add sign up service

--- pre-onboarding-assignment/src/component/sign-in/sign-in.tsx
+++ pre-onboarding-assignment/src/component/sign-in/sign-in.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../css/sign-in.scss';
+import './sign-in.scss';
 
 function SignIn() {
     return (
--- pre-onboarding-assignment/src/component/sign-up/sign-up.tsx
+++ pre-onboarding-assignment/src/component/sign-up/sign-up.tsx
@@ -1,20 +1,81 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../css/sign-up.scss';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import './sign-up.scss';
 
 function SignUp() {
 
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [formCheck, setFormCheck] = useState(false);
+
+    const navigate = useNavigate();
+
+    const emailRegEx = /^[A-Za-z0-9]*@([.]?[A-Za-z0-9])*/;
+    const passwordRegEx = /^[A-Za-z0-9]{8,}$/;
+
+    const emailCheck = (userEmail: string) => {
+        console.log(emailRegEx.test(userEmail))
+        return emailRegEx.test(userEmail);
+    }
+
+    const passwordCheck = (userPassword: string) => {
+        console.log(passwordRegEx.test(userPassword));
+        return passwordRegEx.test(userPassword);
+    }
+
+    function onEmailHandler(e: any) {
+        setEmail(e.target.value);
+        emailCheck(e.target.value);
+        formValidater();
+    }
+
+    function onPasswordHandler(e: any) {
+        setPassword(e.target.value);
+        passwordCheck(e.target.value);
+        formValidater();
+    }
+
+    function formValidater() {
+        if (emailRegEx.test(email) && passwordRegEx.test(password)) {
+            setFormCheck(true);
+        } else {
+            setFormCheck(false);
+        }
+    }
+
+    function onSubmitHandler(e: any) {
+        e.preventDefault();
+
+        fetch("https://pre-onboarding-selection-task.shop/auth/signup", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                email: email,
+                password: password,
+            }),
+        })
+            .then(res => res.json())
+            .then(res => {
+                if (res.access_token) {
+                    alert("회원가입에 성공하였습니다.");
+                    navigate('/');
+                }
+            });
+    }
+
     return (
         <div className="page">
-            <div className="signup-container">
+            <form className="signup-container" onSubmit={onSubmitHandler}>
                 <h1>Sign Up</h1>
                 <label>Email</label>
-                <input type="email" placeholder="이메일을 입력해주세요." />
+                <input type="email" value={email} onChange={onEmailHandler} placeholder="이메일을 입력해주세요." />
                 <label>Password</label>
-                <input type="password" placeholder="비밀번호를 입력해주세요." />
-                <button type="submit">Sign Up</button>
+                <input type="password" value={password} onChange={onPasswordHandler} placeholder="비밀번호를 입력해주세요." />
+                <button type="submit" disabled={!formCheck}>Sign Up</button>
                 <Link to="/" className="link">Back to login page</Link>
-            </div>
+            </form>
         </div>
     );
 }
