97c89f14b69a33ca96b38296cc8dacbd228f3ade
mahanbeom <akgksqja@gmail.com>
2023-01-05 21:21:01 +0900

    feat: Add todo list lookup

--- pre-onboarding-assignment/src/component/todo-list/index.tsx
+++ pre-onboarding-assignment/src/component/todo-list/index.tsx
@@ -1,12 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './todo.scss';
 import TodoCreate from './todo-create';
+import TodoList from './todo-list';
+import { BACKEND_URL } from '../../environment';
 
 function TodoMain() {
 
-
+    const access_token = localStorage.getItem('login-token');
     const navigate = useNavigate();
+    const [todoList, setTodoList] = useState([]);
 
     function tokenVerificaion() {
         if (!localStorage.getItem('login-token')) {
@@ -21,10 +24,26 @@ function TodoMain() {
 
     function listReload() {
         console.log("reload");
+        fetch(`${BACKEND_URL}/todos`, {
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${access_token}`,
+            },
+        })
+            .then(res => res.json())
+            .then(res => {
+                console.log(res);
+                setTodoList(res);
+            })
+            .catch((error) => {
+                alert("To-do list 조회에 실패하였습니다.");
+                console.log(error);
+            });
     }
 
     useEffect(() => {
         tokenVerificaion();
+        listReload();
     }, []);
 
     return (
@@ -35,15 +54,11 @@ function TodoMain() {
             </div>
             <div className='content-container'>
                 <TodoCreate listReload={listReload} />
-                <ul>
-                    <li>
-                        <span>sample data</span>
-                        <div style={{ display: 'flex', gap: '10px' }}>
-                            <button>Edit</button>
-                            <button>Delete</button>
-                        </div>
-                    </li>
-                </ul>
+                {
+                    todoList.map(
+                        (todo: any) => <TodoList key={todo.id} content={todo.todo} />
+                    )
+                }
             </div>
         </div>
     );
--- pre-onboarding-assignment/src/component/todo-list/todo-list.tsx
+++ pre-onboarding-assignment/src/component/todo-list/todo-list.tsx
@@ -1,44 +1,11 @@
-import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import './todo-list.scss';
 
-function TodoList() {
 
-    const navigate = useNavigate();
+function TodoList(props: any) {
 
-    function tokenVerificaion() {
-        if (!localStorage.getItem('login-token')) {
-            return navigate("/");
-        }
-    }
-
-    function onLogoutHandler() {
-        localStorage.removeItem('login-token');
-        return navigate("/");
-    }
-
-    useEffect(() => {
-        tokenVerificaion();
-    }, []);
 
     return (
-        <div className="todo-page">
-            <div className="header">
-                <span>TODOLIST</span>
-                <button onClick={onLogoutHandler}>Logout</button>
-            </div>
-            <div className='content-container'>
-                <h2>Todo List</h2>
-                <ul>
-                    <li>
-                        <span>sample data</span>
-                        <div style={{ display: 'flex', gap: '10px' }}>
-                            <button>Edit</button>
-                            <button>Delete</button>
-                        </div>
-                    </li>
-                </ul>
-            </div>
+        <div>
+            <span>{props.content}</span>
         </div>
     );
 }
